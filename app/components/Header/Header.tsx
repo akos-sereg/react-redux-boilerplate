1da6970dd4567d5136b30060405409b2ead7fbd7
Akos Sereg <akos.sereg@gmail.com>
2022-04-01 21:06:34 +0200

    modernizing Header.tsx

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -3,24 +3,22 @@ import { Link } from 'react-router-dom';
 import Favicon from './images/favicon.ico';
 import './style.scss';
 
-class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <Link to="app" className="navbar-brand">
-            <img alt="Favicon" src={Favicon} height="24" />
-          </Link>
-          <ul className="nav navbar-nav">
+const Header = () => {
+  return (
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <Link to="app" className="navbar-brand">
+          <img alt="Favicon" src={Favicon} height="24" />
+        </Link>
+        <ul className="nav navbar-nav">
 
-            <li><Link to="/app">Home</Link></li>
-            <li><Link to="/authors">Authors</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+          <li><Link to="/app">Home</Link></li>
+          <li><Link to="/authors">Authors</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
